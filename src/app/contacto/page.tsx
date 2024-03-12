72f59674ceca79ae08e14cfa9b8e6ebc1bfca679
Ricardo Coello <ricardodavidquevedo29@gmail.com>
2024-03-12 15:15:59 -0300

    prueba de margenes

--- src/app/contacto/page.tsx
+++ src/app/contacto/page.tsx
@@ -6,7 +6,7 @@ export default function App() {
   const variants = ["flat", "bordered", "underlined", "faded"];
 
   return (
-    <div className="flex flex-col items-center mt-10 h-screen">
+    <div className="flex flex-col items-center mt-10 mb-0 h-screen">
       <div className=" w-1/2 rounded-lg h-96 flex flex-col items-center">
         <h1 className="font-bold text-2xl">Contactanos</h1>
         <form action="">
@@ -14,7 +14,7 @@ export default function App() {
             <Input type="name" variant={"underlined"} color="primary" required label="Nombre" />
             <Input type="number" variant={"underlined"} color="primary" required label="Teléfono" />
             <Input type="email" variant={"underlined"} color="primary" required label="Email" />
-            <Input type="message" variant={"underlined"} color="primary" required className=" text-ellipsis" label="Mensaje" />
+            <Input type="message" variant={"underlined"} color="primary" required label="Mensaje" />
           </div>
           <div className="flex flex-col px-2 sm:px-0 sm:flex-row justify-center mt-5 gap-5">
             <Button className=" w-full sm:w-1/2" color="primary" variant="shadow">
